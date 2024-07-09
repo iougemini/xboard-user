57efcaa1e1c37a3d0bf25cfee7f988907d3980dd
iougemini <163802753+iougemini@users.noreply.github.com>
2024-07-09 15:31:27 +0800

    Create env.js

---
+++ env.js
@@ -0,0 +1,19 @@
+// API地址
+window.routerBase = 'https://sub.niege.us.kg/'
+window.settings = {
+  // 站点名称
+  title: 'Oracle Sub',
+  // 站点描述
+  description: 'a oracle sub site',
+  assets_path: '/assets',
+  // 主题色
+  theme: {
+    color: 'black', //可选default、blue、black、、darkblue
+  },
+  // 版本号
+  version: '0.1.1-dev',
+  // 登陆背景
+  background_url: 'https://gcore.jsdelivr.net/gh/nie365/BingImages@archive/img/latest/1080p.jpg',
+  // 站点LOGO
+  logo: 'https://cdn.linux.do/uploads/default/optimized/3X/f/0/f0eec44920f273f0d066c9acb9ee814f53187e22_2_32x32.png',
+}
