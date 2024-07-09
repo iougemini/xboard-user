a974aa11ce800ee6aa3d16af1003fc2a4354e9c8
iougemini <163802753+iougemini@users.noreply.github.com>
2024-07-09 15:36:59 +0800

    Update env.js

--- env.js
+++ env.js
@@ -13,7 +13,7 @@ window.settings = {
   // 版本号
   version: '0.1.1-dev',
   // 登陆背景
-  background_url: 'https://img1.131213.xyz/file/cfbe091e66cfb7b6e9ce6.png',
+  background_url: 'https://gcore.jsdelivr.net/gh/nie365/BingImages@archive/img/latest/1080p.jpg',
   // 站点LOGO
-  logo: 'https://cdn.linux.do/uploads/default/optimized/3X/f/0/f0eec44920f273f0d066c9acb9ee814f53187e22_2_32x32.png',
+  logo: 'https://img1.131213.xyz/file/cfbe091e66cfb7b6e9ce6.png',
 }
